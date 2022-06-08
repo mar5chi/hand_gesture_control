e1bff51bba0f39695a0cf8aa4b6c282eb82d415e
mar5chi <maschildbeck@gmail.com>
2022-06-08 23:13:14 +0200

    update rendering

--- HandTrackerRenderer.py
+++ HandTrackerRenderer.py
@@ -7,11 +7,6 @@ LINES_HAND = [[0,1],[1,2],[2,3],[3,4],
             [9,13],[13,14],[14,15],[15,16],
             [13,17],[17,18],[18,19],[19,20],[0,17]]
 
-# LINES_BODY to draw the body skeleton when Body Pre Focusing is used
-LINES_BODY = [[4,2],[2,0],[0,1],[1,3],
-            [10,8],[8,6],[6,5],[5,7],[7,9],
-            [6,12],[12,11],[11,5],
-            [12,14],[14,16],[11,13],[13,15]]
 
 class HandTrackerRenderer:
     def __init__(self, 
@@ -35,9 +30,7 @@ class HandTrackerRenderer:
             self.show_rot_rect = False
             self.show_scores = False
 
-        self.show_xyz_zone = self.show_xyz = self.tracker.xyz
         self.show_fps = False
-        self.show_body = False # self.tracker.body_pre_focusing is not None
         self.show_inferences_status = False
 
         if output is None:
@@ -54,40 +47,27 @@ class HandTrackerRenderer:
     def draw_selection(self, todisplay, selection):
         current_selection = 'Your Selection:'
         font = cv2.FONT_HERSHEY_PLAIN
-        #font = cv2.FONT_HERSHEY_COMPLEX_SMALL
         font_thickness = 1
         dist_x = 20
-        #dist = 85
-        dist = 30
-        frame_width = self.tracker.frame_size
-        print(f'frame_width: {frame_width}')
-        # Draw todisplay:
-        # getTextSize (const String &text, int fontFace, double fontScale, int thickness, int *baseLine)
-        #(w, h), _ = cv2.getTextSize(todisplay, cv2.FONT_HERSHEY_PLAIN, 1, 2)
-        #print(f'text_size: ({w}, {h}), {_}')
-        #(w, h), _ = cv2.getTextSize(todisplay, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, 2)
-        #print(f'text_size: ({w}, {h}), {_}')
-        #(w, h), _ = cv2.getTextSize(todisplay, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-        #print(f'text_size: ({w}, {h}), {_}')
+        dist_y = 30
         
         (w, h), _ = cv2.getTextSize(todisplay, font, 1, font_thickness)
-        cv2.rectangle(self.frame, (dist_x, dist-h-5), (dist_x + w, dist+5), (240, 240, 240), -1)
+        cv2.rectangle(self.frame, (dist_x, dist_y-h-5), (dist_x + w, dist_y+5), (240, 240, 240), -1)
         # cv2.putText(image, text, org, font, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
-        cv2.putText(self.frame, todisplay, (dist_x, dist), font, 1, (15,15,15), font_thickness)
-        dist += (h + 13)
+        cv2.putText(self.frame, todisplay, (dist_x, dist_y), font, 1, (15,15,15), font_thickness)
+        dist_y += (h + 13)
         # Draw user selections:
         for k, v in selection.items():
             #current_selection += f' {v}'
             #text = f'{k}: {v}'
             text = v
             (w, h), _ = cv2.getTextSize(text, font, 1, 2)
-            cv2.rectangle(self.frame, (dist_x + 30, dist-h-5), (50 + w, dist+5), (240, 240, 240), -1)
+            cv2.rectangle(self.frame, (dist_x + 30, dist_y-h-5), (50 + w, dist_y+5), (240, 240, 240), -1)
             # cv2.putText(image, text, org, font, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
-            cv2.putText(self.frame, text, (dist_x + 30, dist), font, 1, (15,15,15), font_thickness)
-            dist += (h + 13)
+            cv2.putText(self.frame, text, (dist_x + 30, dist_y), font, 1, (15,15,15), font_thickness)
+            dist_y += (h + 13)
 
     def draw_hand(self, hand):
-
         if self.tracker.use_lm:
             # (info_ref_x, info_ref_y): coords in the image of a reference point 
             # relatively to which hands information (score, handedness, xyz,...) are drawn
@@ -152,26 +132,8 @@ class HandTrackerRenderer:
                 cv2.putText(self.frame, f"Palm score: {hand.pd_score:.2f}", 
                         (x, y), 
                         cv2.FONT_HERSHEY_PLAIN, 2, (255,255,0), 2)
-            
-        if self.show_xyz:
-            if self.tracker.use_lm:
-                x0, y0 = info_ref_x - 40, info_ref_y + 40
-            else:
-                x0, y0 = box_tl[0], box_br[1]+20
-            cv2.rectangle(self.frame, (x0,y0), (x0+100, y0+85), (220,220,240), -1)
-            cv2.putText(self.frame, f"X:{hand.xyz[0]/10:3.0f} cm", (x0+10, y0+20), cv2.FONT_HERSHEY_PLAIN, 1, (20,180,0), 2)
-            cv2.putText(self.frame, f"Y:{hand.xyz[1]/10:3.0f} cm", (x0+10, y0+45), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 2)
-            cv2.putText(self.frame, f"Z:{hand.xyz[2]/10:3.0f} cm", (x0+10, y0+70), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
-        if self.show_xyz_zone:
-            # Show zone on which the spatial data were calculated
-            cv2.rectangle(self.frame, tuple(hand.xyz_zone[0:2]), tuple(hand.xyz_zone[2:4]), (180,0,180), 2)
-
-    def draw_body(self, body):
-        lines = [np.array([body.keypoints[point] for point in line]) for line in LINES_BODY if body.scores[line[0]] > self.tracker.body_score_thresh and body.scores[line[1]] > self.tracker.body_score_thresh]
-        cv2.polylines(self.frame, lines, False, (255, 144, 30), 2, cv2.LINE_AA)
 
     def draw_bag(self, bag):
-        
         if self.show_inferences_status:
             # Draw inferences status
             h = self.frame.shape[0]
@@ -184,17 +146,6 @@ class HandTrackerRenderer:
             nb_lm_inferences = bag.get("lm_inference", 0)
             if nb_lm_inferences:
                 cv2.rectangle(self.frame, (3*u, 8*u), ((3+nb_lm_inferences)*u, 9*u), (0,0,255), -1)
-
-        body = bag.get("body", False)
-        if body and self.show_body:
-            # Draw skeleton
-            self.draw_body(body)
-            # Draw Movenet smart cropping rectangle
-            cv2.rectangle(self.frame, (body.crop_region.xmin, body.crop_region.ymin), (body.crop_region.xmax, body.crop_region.ymax), (0,255,255), 2)
-            # Draw focus zone
-            focus_zone= bag.get("focus_zone", None)
-            if focus_zone:
-                cv2.rectangle(self.frame, focus_zone[0:2], focus_zone[2:4], (0,255,0),2)
 
     def draw(self, frame, hands, todisplay, selection, bag={}):
         self.frame = frame
@@ -238,17 +189,8 @@ class HandTrackerRenderer:
         elif key == ord('7') and self.tracker.use_lm:
             if self.tracker.use_gesture:
                 self.show_gesture = not self.show_gesture
-        elif key == ord('8'):
-            if self.tracker.xyz:
-                self.show_xyz = not self.show_xyz    
-        elif key == ord('9'):
-            if self.tracker.xyz:
-                self.show_xyz_zone = not self.show_xyz_zone 
         elif key == ord('f'):
             self.show_fps = not self.show_fps
-        elif key == ord('b'):
-            if self.tracker.body_pre_focusing:
-                self.show_body = not self.show_body 
         elif key == ord('s'):
             self.show_inferences_status = not self.show_inferences_status
         return key
